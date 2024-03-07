54a9f894a102fabefbc3986c8a2aee0c5a4e8ebc
vanphutin <vanphutin2902@gmail.com>
2024-03-07 19:43:08 +0700

    init project

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+// can use : class component or function component
+import React from 'react';
+
+class MyComponent extends React.Component{
+    //JSX 
+    render(){
+        return(
+            <div>My first component</div>
+        );
+    }
+}
+
+export default MyComponent;

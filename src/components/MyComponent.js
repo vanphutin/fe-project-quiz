4161d88a6d2778ad3d3b13e150f6cf4d35bad4a0
vanphutin <vanphutin2902@gmail.com>
2024-03-10 16:13:28 +0700

    Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,51 +1,16 @@
 // can use : class component or function component
 import React from 'react';
+import UserInfo from './userInfo';
 
 class MyComponent extends React.Component{
-    state = {
-        name : 'Van Phu Tin',
-        address : 'Tin FullStack',
-        age : 21
-    }
-    handleClick = (event) => { //dùng arowFunction để fix lỗi 
-        console.log('click me my button');
-
-        //merge state => react class
-        this.setState({
-            name : 'Tin Devoloper',
-            age : Math.floor((Math.random() * 100) + 1)
-        })
-
-           
-        
-    }
-    handOnMouseOver(event){
-        console.log(event);
-    }
-    handleOnChangeInput(event){
-        this.setState({
-            name : event.target.value
-        })
-    }
-    handleOnChangeSubmit = (event) =>{
-        event.preventDefault();
-        console.log(this.state);
-    }
+    
     //JSX 
     render(){
         return(
             <div>
-                My name is {this.state.name} and I {this.state.age} old
-                <button onClick={this.handleClick}>click me</button>
-                <button onMouseOver={this.handOnMouseOver}>Hover me</button>
-                
-                <form action="" onSubmit={(event) => {this.handleOnChangeSubmit(event)}}>
-                    <input type="text" placeholder='what your name ?'
-                    onChange={(event) =>this.handleOnChangeInput(event)}
-                     />
-                    <button type='submit'>submit</button>
-                </form>
-                </div>
+                <UserInfo />    
+
+            </div>
         );
     }
 }

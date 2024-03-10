847ba6b65756c95e5223c7810f45f69cefa2586c
vanphutin <vanphutin2902@gmail.com>
2024-03-10 18:10:20 +0700

    outputting list

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,14 +3,23 @@ import UserInfo from "./userInfo";
 class DisplayInfo extends React.Component {
   render() {
     //props => properties
-    const { name, age } = this.props;
+    const { listUser } = this.props;
     return (
       <div>
-        <br />
+        {listUser.map((user) => {
+          return (
+            <div key={user.ID}>
+              <div>my name is {user.name} </div>
+              <div> i am {user.age} year old</div>
+              <hr />
+            </div>
+          );
+        })}
+        {/* <br />
         <div>Display list user: </div>
         <hr />
         <div>my name is {this.props.name}</div>
-        <div> i am {age} year old</div>
+        <div> i am {age} year old</div> */}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,12 +4,19 @@ import UserInfo from "./userInfo";
 import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
+  state = {
+    listUser: [
+      { ID: 1, name: "van phu tin", age: "18" },
+      { ID: 2, name: "tin dev", age: "21" },
+      { ID: 3, name: "tin fullStack", age: "22" },
+    ],
+  };
   //JSX
   render() {
     return (
       <div>
         <UserInfo />
-        <DisplayInfo name="Van Phu Tin" age="21" />
+        <DisplayInfo listUser={this.state.listUser} />
       </div>
     );
   }

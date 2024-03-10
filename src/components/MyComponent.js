5394a67388b92ac00b7d477f5772c3b05c3d43fd
vanphutin <vanphutin2902@gmail.com>
2024-03-10 14:59:35 +0700

    video DOM EVENT

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,20 @@ class MyComponent extends React.Component{
         address : 'Tin FullStack',
         age : 21
     }
+    handleClick(event){
+        console.log('click me my button');
+        console.log(' My name is ',this.state.name);
+    }
+    handOnMouseOver(event){
+        console.log(event);
+    }
     //JSX 
     render(){
         return(
             <div>
                 My name is {this.state.name} and I'm from Quang Nam
+                <button onClick={this.handleClick}>click me</button>
+                <button onMouseOver={this.handOnMouseOver}>Hover me</button>
             </div>
         );
     }

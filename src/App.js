ccdf54a3905e374e46026ce8d43fb149cc91da05
vanphutin <vanphutin2902@gmail.com>
2024-03-16 17:16:02 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello word
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
+import Header from "./components/Header/Header";
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <b>Tín</b> - <i>FullStack</i>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">User</Nav.Link>
+            <Nav.Link href="#home">Admin</Nav.Link>
+          </Nav>
+          <nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+            </NavDropdown>
+          </nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
